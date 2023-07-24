b5df34b73e9850592c6af689c7831827012ef31e
TimLaine <timothee.laine@gmail.com>
2023-07-24 09:54:31 +0200

    Finished exo05, using native functions of js like isNaN() and toFixed()

--- Exo05/app.js
+++ Exo05/app.js
@@ -1,3 +1,12 @@
-function test(){
-    console.log("test")
+let result = document.querySelector("#result")
+
+// This function get the value in input then check if it's a numeric value or not. If it isn't, it answers an alert. If it is, it convert the number into its value in francs.
+function conversion(){
+    const valeur = document.querySelector("input").value
+    if(isNaN(valeur)){
+        result.innerText = "Veuillez entrer une valeur numérique"
+    } else {
+        let conversion = valeur*6.55957
+        result.innerText = conversion.toFixed(2)+" francs"
+    }
 }
